95dc0bb6ef8b08e6af96df1fa65b9902471b5375
Daniel-MSoares <munizsoaresrapper@gmail.com>
2022-05-29 18:39:28 -0300

    mobile Simples v1.0

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function showTimeline(idDiv){
+    let divLogica= document.getElementById(`box_logica`);
+    let divCbi= document.getElementById(`box_cbi`);
+    let menu= document.getElementById(`box_btn`);
+    document.getElementById(`${idDiv}`).style.display='block'
+    hideMenu()
+}
+function hideMenu(){
+    
+}
+
+function showMenu(){
+    
+}
